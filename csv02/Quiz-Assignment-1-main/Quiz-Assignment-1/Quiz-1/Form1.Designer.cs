57b3b64247dd744f8b156b5ebd725f8e42e7c048
Makeyabe <96247134+Bowsorsawan@users.noreply.github.com>
2022-02-21 00:05:39 +0700

    01

---
+++ csv02/Quiz-Assignment-1-main/Quiz-Assignment-1/Quiz-1/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Quiz_Assignment_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Columnรายการ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnรายรับ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Columnรายจ่าย = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxSumExpenses = new System.Windows.Forms.TextBox();
+            this.textBoxSumIncome = new System.Windows.Forms.TextBox();
+            this.textBoxList = new System.Windows.Forms.TextBox();
+            this.textBoxIncome = new System.Windows.Forms.TextBox();
+            this.textBoxExpenses = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(914, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Columnรายการ,
+            this.Columnรายรับ,
+            this.Columnรายจ่าย});
+            this.dataGridView1.Location = new System.Drawing.Point(371, 44);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(406, 381);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Columnรายการ
+            // 
+            this.Columnรายการ.HeaderText = "รายการ";
+            this.Columnรายการ.MinimumWidth = 8;
+            this.Columnรายการ.Name = "Columnรายการ";
+            this.Columnรายการ.Width = 150;
+            // 
+            // Columnรายรับ
+            // 
+            this.Columnรายรับ.HeaderText = "รายรับ";
+            this.Columnรายรับ.MinimumWidth = 8;
+            this.Columnรายรับ.Name = "Columnรายรับ";
+            this.Columnรายรับ.Width = 150;
+            // 
+            // Columnรายจ่าย
+            // 
+            this.Columnรายจ่าย.HeaderText = "รายจ่าย";
+            this.Columnรายจ่าย.MinimumWidth = 8;
+            this.Columnรายจ่าย.Name = "Columnรายจ่าย";
+            this.Columnรายจ่าย.Width = 150;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxSumExpenses);
+            this.groupBox1.Controls.Add(this.textBoxSumIncome);
+            this.groupBox1.Location = new System.Drawing.Point(29, 286);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(284, 145);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "รวม";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 71);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "รายจ่าย";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 31);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "รายรับ";
+            // 
+            // textBoxSumExpenses
+            // 
+            this.textBoxSumExpenses.Location = new System.Drawing.Point(113, 66);
+            this.textBoxSumExpenses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxSumExpenses.Name = "textBoxSumExpenses";
+            this.textBoxSumExpenses.Size = new System.Drawing.Size(121, 27);
+            this.textBoxSumExpenses.TabIndex = 1;
+            // 
+            // textBoxSumIncome
+            // 
+            this.textBoxSumIncome.Location = new System.Drawing.Point(113, 24);
+            this.textBoxSumIncome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxSumIncome.Name = "textBoxSumIncome";
+            this.textBoxSumIncome.Size = new System.Drawing.Size(121, 27);
+            this.textBoxSumIncome.TabIndex = 0;
+            // 
+            // textBoxList
+            // 
+            this.textBoxList.Location = new System.Drawing.Point(144, 44);
+            this.textBoxList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxList.Name = "textBoxList";
+            this.textBoxList.Size = new System.Drawing.Size(121, 27);
+            this.textBoxList.TabIndex = 3;
+            // 
+            // textBoxIncome
+            // 
+            this.textBoxIncome.Location = new System.Drawing.Point(144, 106);
+            this.textBoxIncome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxIncome.Name = "textBoxIncome";
+            this.textBoxIncome.Size = new System.Drawing.Size(121, 27);
+            this.textBoxIncome.TabIndex = 4;
+            // 
+            // textBoxExpenses
+            // 
+            this.textBoxExpenses.Location = new System.Drawing.Point(144, 170);
+            this.textBoxExpenses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxExpenses.Name = "textBoxExpenses";
+            this.textBoxExpenses.Size = new System.Drawing.Size(121, 27);
+            this.textBoxExpenses.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "รายการ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "เงินรายรับ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "เงินรายจ่าย";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(151, 210);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(90, 27);
+            this.buttonAdd.TabIndex = 9;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(151, 254);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(90, 27);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 459);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxExpenses);
+            this.Controls.Add(this.textBoxIncome);
+            this.Controls.Add(this.textBoxList);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Columnรายการ;
+        private DataGridViewTextBoxColumn Columnรายรับ;
+        private DataGridViewTextBoxColumn Columnรายจ่าย;
+        private GroupBox groupBox1;
+        private Label label5;
+        private Label label4;
+        private TextBox textBoxSumExpenses;
+        private TextBox textBoxSumIncome;
+        private TextBox textBoxList;
+        private TextBox textBoxIncome;
+        private TextBox textBoxExpenses;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button buttonAdd;
+        private Button buttonSave;
+    }
+}
